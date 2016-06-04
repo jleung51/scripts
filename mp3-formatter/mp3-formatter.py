fccd1f47f328c33f19f9f51a347c462be14acf48
jleung51 <jleung513@gmail.com>
2016-06-03 23:37:19 -0700

    MP3: Separate file list pruning for clarity

--- mp3-formatter/mp3-formatter.py
+++ mp3-formatter/mp3-formatter.py
@@ -8,10 +8,17 @@ mp3_extension = ".mp3"
 files = os.listdir('.')
 
 for f in files:
+
+    # Prune directories
     if not os.path.isfile(f):
         files.remove(f)
-    else:
-        f = os.path.abspath(f)
+        continue
+
+    # Prune this file
+    f_temp = os.path.abspath(f)
+    if f_temp == os.path.abspath(__file__):
+        files.remove(f)
+        continue
 
 for f in files:
     print(f)
