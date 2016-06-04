a6b951ab3572fe28b7cceb89196206b9be483db7
jleung51 <jleung513@gmail.com>
2016-06-04 16:30:36 -0700

    MP3: Document main shell script

--- mp3-formatter/mp3_formatter.sh
+++ mp3-formatter/mp3_formatter.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-./url_scrape_div.py $1 | ./rename_mp3.py
+# This sh script scrapes a tracklist from www.hikarinoakariost.info
+# (don't judge me) and renames all the MP3 files in the current directory
+# (assumed to be organized in correct track order) correspondingly.
+
+if [ "$1" = "--help" ] ; then
+    echo "Usage: ./mp3_formatter.sh URL_TO_TRACKLIST"
+else
+    ./url_scrape_div.py $1 | ./rename_mp3.py
+fi
