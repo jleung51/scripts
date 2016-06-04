095b8da181e667cf8268b68d5e95e3171da8232c
jleung51 <jleung513@gmail.com>
2016-06-04 00:13:02 -0700

    MP3: Flip method of pruning incorrect files
    The opposite was giving me odd and inexplicable results, like files
    which should have been ignored but weren't.

--- mp3-formatter/mp3-formatter.py
+++ mp3-formatter/mp3-formatter.py
@@ -4,27 +4,28 @@ import ID3
 import os
 
 mp3_extension = ".mp3"
+names = ["final_name_1", "final_name_2", "final_name_3"]
 
-files = os.listdir('.')
+files_all = os.listdir('.')
+files = []
 
-for f in files:
+for f in files_all:
 
     # Prune directories
     if not os.path.isfile(f):
-        files.remove(f)
         continue
 
     # Prune non-MP3 files
     filename, extension = os.path.splitext(f)
     if extension != mp3_extension:
-        files.remove(f)
         continue
 
     # Prune this file
     f_temp = os.path.abspath(f)
     if f_temp == os.path.abspath(__file__):
-        files.remove(f)
         continue
+
+    files.append(f)
 
 files.sort()
 
