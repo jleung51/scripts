2916ec4ea722e308c48816b4ab134282516dc43f
jleung51 <jleung513@gmail.com>
2016-06-19 16:32:02 -0700

    Linux-setup: Add guake autostart configuration

--- setup_linux.sh
+++ setup_linux.sh
@@ -8,7 +8,7 @@ sudo apt-get -y install guake
 gsettings set org.gnome.desktop.default-applications.terminal exec 'guake --new-tab'
 gsettings set org.gnome.desktop.default-applications.terminal exec-arg '-e'
 sudo cp /usr/share/applications/guake.desktop /etc/xdg/autostart/
-cp guake.desktop ~/.config/autostart/guake.desktop
+cp files/guake.desktop ~/.config/autostart/
 
 sudo apt-get -y install xubuntu-community-wallpapers
 mkdir ~/Pictures/Wallpapers
