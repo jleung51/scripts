25cb0279c400ad4e1578f588c9f31ece8d44f49e
jleung51 <jleung513@gmail.com>
2016-06-19 16:52:02 -0700

    Linux-setup: Move manual-setup instructions to subscript

--- setup_linux.sh
+++ setup_linux.sh
@@ -10,16 +10,4 @@ sudo apt-get -y install curl git vim
 ./setup_wallpaper.sh
 # ./setup_wallpaper_xubuntu.sh
 
-# Manual
-#guake --preferences
-#	General:
-#		Disable popup notifications on startup
-#	Shell:
-#		Set default interpreter to /bin/zsh
-#	Appearance:
-#		Disable "Use the system fixed width font"
-#		Select Font: "Meslo LG M DZ for Powerline RegularForPowerline"
-#		Select Palette - Built-in scheme: "Tomorrow Night Blue"
-#		Set Transparency: 25%
-#
-#http://askubuntu.com/questions/412982/where-can-i-download-xubuntu-released-wallpapers
+./setup_manual_instructions.sh
