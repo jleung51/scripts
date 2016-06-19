fdeedd82d92aaa5647d0bb532a8380281294e87d
jleung51 <jleung513@gmail.com>
2016-06-19 16:57:16 -0700

    Linux-setup: Add tasteful spacing

--- sub-scripts/setup_ohmyzsh_agnoster.sh
+++ sub-scripts/setup_ohmyzsh_agnoster.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 sudo apt-get -y install zsh
+
 # oh-my-zsh
 sudo apt-get -y install curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sed -i 's/ZSH_THEME.*/ZSH_THEME="agnoster"/g' ~/.zshrc
+
 # agnoster font
 sudo wget -P /usr/share/fonts/opentype https://github.com/powerline/fonts/raw/master/Meslo/Meslo%20LG%20M%20DZ%20Regular%20for%20Powerline.otf
 fc-cache -fv
--- sub-scripts/setup_wallpaper.sh
+++ sub-scripts/setup_wallpaper.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 sudo apt-get -y install xubuntu-community-wallpapers
+
 mkdir ~/Pictures/Wallpapers/
 cp /usr/share/xfce4/backdrops/Mountainous_View_by_Sven_Scheuermier.jpg ~/Pictures/Wallpapers/
+
 gsettings set org.gnome.desktop.background picture-uri file://~/Pictures/Wallpapers/Mountainous_View_by_Sven_Scheuermier.jpg
