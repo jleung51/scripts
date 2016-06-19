aa003af53f6b6a22a401ebc976acb200f7e0346e
jleung51 <jleung513@gmail.com>
2016-06-19 16:55:03 -0700

    Linux setup: Move sub-scripts to separate directory

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+SCRIPTS="./sub-scripts"
+
 cd ~/Downloads/
 
 sudo apt-get -y install curl git vim
 
-./setup_guake.sh
-./setup_ohmyzsh_agnoster.sh
+$SCRIPTS/setup_guake.sh
+$SCRIPTS/setup_ohmyzsh_agnoster.sh
 
-./setup_wallpaper.sh
-# ./setup_wallpaper_xubuntu.sh
+$SCRIPTS/setup_wallpaper.sh
+# $SCRIPTS/setup_wallpaper_xubuntu.sh
 
-./setup_manual_instructions.sh
+$SCRIPTS/setup_manual_instructions.sh
