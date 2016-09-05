8c1515fac40fe970fe50dee325ec730ddf3b4800
Jeffrey Leung <jleung513@gmail.com>
2016-09-05 14:08:47 -0700

    [Servers] Add documentation

--- open-port.sh
+++ open-port.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-# For more information, see https://www.digitalocean.com/community/tutorials/how-to-list-and-delete-iptables-firewall-rules
+#
+# This shell script opens a local port by adding an exception to the
+# firewall rules.
+#
+# For more information, see:
+#   https://www.digitalocean.com/community/tutorials/how-to-list-and-delete-iptables-firewall-rules
+#
 
 if [ $# -ge 1 ] ; then
   sudo iptables -I INPUT -p tcp --dport $1 --syn -j ACCEPT
---
+++ services/README.md
@@ -0,0 +1,3 @@
+# Services
+
+This directory contains a template to create a custom Linux service, as well as scripts to activate or deactivate it upon autostart.
