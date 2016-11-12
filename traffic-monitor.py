dbb52a6959a9f1f7c2d527d821aca62975fe1923
Jeffrey Leung <jleung513@gmail.com>
2016-11-11 23:52:20 -0800

    Format severity/type levels and description

--- traffic-monitor.py
+++ traffic-monitor.py
@@ -14,10 +14,50 @@ bing_maps_auth_key = ""
 coordinate_southwest = "45.219, -122.325"
 coordinate_northeast = "46.610, -122.107"
 
-# For the possible values and their explanations, see
-# https://msdn.microsoft.com/en-ca/library/hh441726.aspx
+# See below for the meanings of the severity levels and types.
+# Keep only the security levels and types useful to you.
+# See https://msdn.microsoft.com/en-ca/library/hh441726.aspx
 severity = "1, 2, 3, 4"
 type = "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11"
+
+severity_list = {
+    "1" : "Low impact",
+    "2" : "Minor",
+    "3" : "Moderate",
+    "4" : "Serious"
+}
+
+type_list = {
+    "1" : "Accident",
+    "2" : "Congestion",
+    "3" : "Disabled vehicle",
+    "4" : "Mass transit",
+    "5" : "Uncategorized alert",
+    "6" : "Uncategorized alert",
+    "7" : "Planned event",
+    "8" : "Road hazard",
+    "9" : "Construction",
+    "10" : "General alert",
+    "11" : "Weather alert"
+}
+
+def decode_severity(severity):
+    string_severity = severity_list[str(severity)]
+    if string_severity == None:
+        raise ValueError(
+                "An unparseable severity level was found (" +
+                str(severity) + ")"
+        )
+    return string_severity
+
+def decode_type(type):
+    string_type = type_list[str(type)]
+    if string_type == None:
+        raise ValueError(
+                "An unparseable type level was found (" +
+                str(type) + ")"
+        )
+    return string_type
 
 def string_list_from(original_list):
     string_list = []
@@ -44,11 +84,21 @@ def alert_for_incidents(response_body):
     incidents = incidents_container[0].get("resources")
 
     for i in incidents:
-        print(str(i.get("severity")) + ' ' + str(i.get("type")))
-        print(i.get("description"))
+        coordinates_float = i.get("point").get("coordinates")
+        coordinates = '(' + ", ".join(string_list_from(coordinates_float)) + ')'
+        print(
+                "Alert: " +
+                decode_severity(i.get("severity")) + " traffic disruption"
+        )
 
-        coordinates = i.get("point").get("coordinates")
-        print('(' + ", ".join(string_list_from(coordinates)) + ')')
+        description = i.get("description")
+        description = description[0].lower() + description[1:]
+        print(
+                decode_type(i.get("type")) + " " +
+                description + " Coordinates: " +
+                coordinates + "."
+        )
+
         print()
 
 if __name__ == "__main__":
