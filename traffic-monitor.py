9b4caf2c98e5a99de7f882c8301c3636d53655c3
Jeffrey Leung <jleung513@gmail.com>
2016-11-12 20:55:04 -0800

    Add email notifications using the Gmail API

--- traffic-monitor.py
+++ traffic-monitor.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
+import argparse
+import base64
 import json
+import os
 import requests
+
+from argparse import Namespace
+from email.mime.text import MIMEText
+from httplib2 import Http
+
+# Gmail API
+from apiclient.discovery import build
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+from oauth2client.service_account import ServiceAccountCredentials
 
 # Configuration
 
@@ -40,6 +54,10 @@ type_list = {
     "10" : "General alert",
     "11" : "Weather alert"
 }
+
+mail_source_application_name = "Traffic Monitor"
+mail_source_email = "gmail_source@email.com"
+mail_target_email = "email_target@gmail.com"
 
 def decode_severity(severity):
     string_severity = severity_list[str(severity)]
@@ -82,7 +100,6 @@ def alert_for_incidents(response_body):
     if len(incidents_container) is 0:
         return
     incidents = incidents_container[0].get("resources")
-
     for i in incidents:
         coordinates_float = i.get("point").get("coordinates")
         coordinates = '(' + ", ".join(string_list_from(coordinates_float)) + ')'
@@ -101,8 +118,49 @@ def alert_for_incidents(response_body):
 
         print()
 
+def get_gmail_credentials():
+    credential_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "generated_credentials.json"
+    )
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(
+                "client_secret.json",
+                "https://www.googleapis.com/auth/gmail.send"
+        )
+        flow.user_agent = mail_source_application_name
+
+        flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+        flags.noauth_local_webserver = True
+        credentials = tools.run_flow(flow, store, flags)
+    return credentials
+
+def create_message(sender, recipient, subject, message_text):
+    message = MIMEText(message_text)
+    message["from"] = sender
+    message["to"] = recipient
+    message["subject"] = subject
+    return {"raw":
+            base64.urlsafe_b64encode(
+                    message.as_string().encode()
+            ).decode("utf-8")
+    }
+
+def alert_to_mail(subject, message):
+    http_auth = get_gmail_credentials().authorize(Http())
+    service = build("gmail", "v1", http=http_auth)
+
+    mail = create_message(mail_source_email, mail_target_email, subject, message)
+    response = service.users().messages() \
+            .send(userId=mail_source_email, body=mail).execute()
+
+    print(json.dumps(response, sort_keys=True, indent=4))
+
 if __name__ == "__main__":
     response = get_traffic_data()
     response_body = response.json()
 
     alert_for_incidents(response_body)
+    alert_to_mail("test_subject", "test_body")
