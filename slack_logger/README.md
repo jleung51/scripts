7ef9fccacd2a98bed17995f96e497d486b894736
Jeffrey Leung <jleung513@gmail.com>
2016-12-24 22:31:47 -0800

    [Slack Logger] Add script

---
+++ slack_logger/README.md
@@ -0,0 +1,49 @@
+# Slack Logger
+
+Python 3 module which provides easy functionality for customized operation status reports to a Slack channel.
+
+## Example
+
+The Slack message will be displayed as follows:
+
+> **Custom Slackbot Name**
+> > _2016-12-23 20:20:28_  
+> > Operation status: OPERATION_STATUS  
+> > MESSAGE_TEXT
+
+## Setup
+
+### Dependencies
+
+Install the following pip 3 dependency:
+* slackclient
+
+### Slack Setup
+
+[Create a Slackbot bot](https://api.slack.com/bot-users) in the Slack team you want to send Slack messages to. Once you are finished the setup, navigate to the _Integration Settings_ section (if you lost the page, it's located at `App Directory > Browse Apps > Custom Integrations > Bots`) and save the API Token displayed there. You will need this API token to authenticate your bot.
+
+Invite your Slack bot to the channel they should send a message to.
+
+### Reporting from the Program
+
+Add the `slack_logger.py` module to the directory of the Python program which needs to log reports to Slack. Import the module in your program:
+
+```
+from slack_logger import SlackLogger
+```
+
+Instantiate a SlackLogger object:
+```
+slack_logger = SlackLogger(slack_api_token, slack_channel, slackbot_name)
+```
+
+And send a report to the Slack team:
+```
+slack_logger.report(operation_status, message_text)
+```
+
+For detailed information on the parameters and usage, see the documentation in the `SlackLogger` class.
+
+### Configuration
+
+If you want to enable debug logging output, change the value of the variable `print_debug_logs` in `slack_logger.py` to True.
---
+++ slack_logger/slack_logger.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+#
+# This Python 3 module provides a function to access the Slack API in order
+# to send a log or operation report to a Slack team.
+
+import time
+from slackclient import SlackClient
+
+# Change this to True to enable output debug logging for this module.
+print_debug_logs = False
+
+def log(log_level, message):
+    if print_debug_logs:
+        print(
+                "[ " +
+                time.strftime("%Y-%m-%d %H:%M:%S") +
+                " | " +
+                log_level +
+                " ] " +
+                message
+        )
+
+def log_debug(message):
+    log("DEBUG  ", message)
+
+def log_success(message):
+    log("SUCCESS", message)
+
+def log_error(message):
+    log("ERROR  ", message)
+
+class SlackLogger:
+
+    def __init__(self, slack_token, channel, slackbot_name):
+        """Instantiates an Slack logging object.
+
+        Parameters:
+        slack_token -- String. The API token of the Slackbot (see
+            README:Setup:Slack Setup)
+            E.g. "xoxb-128312731823-FN3190FHDFK1L1099813UH10"
+        channel -- String. The name of the channel (without a "#").
+            E.g. "random"
+        slackbot_name -- String. The name of the Slackbot user which will send
+            the message. E.g. "Traffic Monitor Slackbot"
+        """
+
+        self.slack_token = slack_token
+        self.channel = channel
+        self.slackbot_name = slackbot_name
+
+    def report(self, operation_status, message_text):
+        """Sends a report to the Slackbot configured during instantiation.
+
+        Parameters:
+        operation_status -- String. The general message to summarize the report.
+            E.g. "SUCCESS" or "*ERROR*"
+        message_text -- String. The detailed report message.
+            E.g. "Traffic accident on Kingsway and Patterson."
+
+        Does not throw exceptions; outputs any error messages.
+        For an example report, see:
+        https://github.com/jleung51/scripts/blob/master/slack_logger/README.md
+        """
+
+        try:
+            SlackClient(self.slack_token).api_call(
+                    "chat.postMessage",
+                    channel = "#" + self.channel,
+                    link_names = 1,
+                    username = self.slackbot_name,
+                    text = ">>> _" + time.strftime("%Y-%m-%d %H:%M:%S") + '_' +
+                            '\n' + "Operation status: " + operation_status +
+                            '\n' + message_text
+            )
+            log_success("Module slack_logger | Slack report sent.")
+        except Exception as e:
+            log_error("Module slack_logger | Error: Slack report not sent.")
+            raise
