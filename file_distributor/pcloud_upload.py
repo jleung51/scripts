f08f483c81f4fa4df45ce347decee2cfb6418101
Jeffrey Leung <jleung513@gmail.com>
2017-01-29 02:33:37 -0800

    [FileDist] Add pCloud file upload functionality

--- file_distributor/pcloud_upload.py
+++ file_distributor/pcloud_upload.py
@@ -20,6 +20,8 @@ file_path_local = config[section_local]["file_path"]
 section_pcloud = "pCloud"
 username = config[section_pcloud]["username"]
 password = config[section_pcloud]["password"]
+dir_path_pcloud = config[section_pcloud]["dir_path"]
+file_name_pcloud = config[section_pcloud]["file_name"]
 
 def log(log_level, message):
     if print_debug_logs:
@@ -48,12 +50,15 @@ class pcloud:
     def __init__(self):
         self.auth_token = None
 
-    def _api_call(self, rest_api, url, params=None,
+    def _api_call(self, rest_api, url, params=None, file_path_upload=None,
             response_body_validity_check=None):
         """Makes a REST API call designed for the pCloud service.
 
         The authentication token will automatically be added to the parameters,
         if it exists.
+        Parameters:
+        file_path_upload -- String. If passed, the request will be a
+            POST request; else, the request will be a GET request.
         """
         if params is None:
             params = dict()
@@ -61,7 +66,12 @@ class pcloud:
             response_body_validity_check = True
         params["auth"] = self.auth_token
 
-        response = requests.get(url=url, params=params)
+        if file_path_upload is None:
+            response = requests.get(url=url, params=params)
+        else:
+            response = requests.post(url=url, params=params,
+                    files={file_path_upload: open(file_path_upload, "rb")}
+            )
         response_body = response.json()
 
         log_debug(rest_api + " status code: " + str(response.status_code))
@@ -101,14 +111,36 @@ class pcloud:
                 passworddigest = password_digest
         )
         response_body = self._api_call(
-                "GET /userinfo", "https://api.pcloud.com/userinfo",
-                request_params, lambda response_body:
+                "GET /userinfo",
+                "https://api.pcloud.com/userinfo",
+                request_params,
+                response_body_validity_check=lambda response_body:
                         response_body is not None and
                         response_body["auth"] is not None
         )
 
         self.auth_token = response_body["auth"]
         log_debug("Successfully logged in.")
+
+    def upload_file(self, file_path_local, dir_path_pcloud, file_name_pcloud):
+        if dir_path_pcloud == "":  # Will be empty string for root folder
+            dir_path_pcloud = None
+
+        request_params = dict(
+                path = dir_path_pcloud,
+                filename = file_name_pcloud,
+                nopartial = 1
+        )
+        self._api_call(
+                "POST /uploadfile", "https://api.pcloud.com/uploadfile",
+                request_params, file_path_local, lambda response_body:
+                        response_body is not None and
+                        response_body["result"] is not None and
+                        response_body["result"] == 0 and
+                        response_body["fileids"] is not None and
+                        len(response_body["fileids"]) > 0
+        )
+        log_success("File " + file_name_pcloud + " uploaded.")
 
     def logout(self):
         if self.auth_token is None:
@@ -127,4 +159,5 @@ class pcloud:
 if __name__ == "__main__":
     p = pcloud()
     p.login()
+    p.upload_file(file_path_local, dir_path_pcloud, file_name_pcloud)
     p.logout()
