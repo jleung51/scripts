0d00951dae0b3492be540aa9e9732e3d9f0f1d25
Jeffrey Leung <jleung513@gmail.com>
2017-02-03 21:33:06 -0800

    [FileDist] Add script with Google Drive authentication
    The script does not yet upload any files to Google Drive.

---
+++ file_distributor/gdrive_upload.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+import argparse
+import configparser
+import httplib2
+import os
+import sys
+import time
+
+# Google Drive API
+from apiclient.discovery import build
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+config_filename = "file_distributor.cfg"
+
+class Logger:
+    """Outputs formatted log messages."""
+
+    # Change this to True to enable output debug logging for this module.
+    print_debug_logs = True
+
+    @classmethod
+    def __log(self, log_level, message):
+        """Outputs a formatted log message if logging is activated.
+
+        Parameters:
+        log_level -- String. Severity of the log message.
+        message -- String. Message to be logged.
+        """
+        if self.print_debug_logs:
+            print(
+                    "[ " +
+                    time.strftime("%Y-%m-%d %H:%M:%S") +
+                    " | " +
+                    log_level +
+                    " ] " +
+                    message
+            )
+
+    @staticmethod
+    def debug(message):
+        """Outputs a debug level log message."""
+        Logger.__log("DEBUG  ", message)
+
+    @staticmethod
+    def success(message):
+        """Outputs a success level log message."""
+        Logger.__log("SUCCESS", message)
+
+    @staticmethod
+    def log_error(message):
+        """Outputs a error level log message."""
+        Logger.__log("ERROR  ", message)
+
+class GoogleDrive:
+
+    def __get_credentials(self):
+        credential_path = os.path.join(
+                os.path.dirname(os.path.realpath(__file__)),
+                "generated_credentials.json"
+        )
+        store = Storage(credential_path)
+        credentials = store.get()
+        if not credentials or credentials.invalid:
+            scope = "https://www.googleapis.com/auth/drive"
+            flow = client.flow_from_clientsecrets(
+                    self.__client_secret_file_path, scope
+            )
+            flow.user_agent = self.__application_name
+
+            flags = argparse \
+                    .ArgumentParser(parents=[tools.argparser]) \
+                    .parse_args()
+            flags.noauth_local_webserver = True
+            credentials = tools.run_flow(flow, store, flags)
+            Logger.debug("Credentials saved to [" + credential_path + "]")
+        return credentials
+
+    def __init__(self, application_name, client_secret_file_path):
+        self.__application_name = application_name
+        self.__client_secret_file_path = client_secret_file_path
+
+        self.__get_credentials()
+
+    def upload_file(self):
+        http_auth = self.__get_credentials().authorize(httplib2.Http())
+        service = build("drive", "v2", http=http_auth)
+
+        # TODO: Add file upload functionality
+
+        Logger.debug("File uploaded.")
+
+if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read(config_filename)
+
+    section_local = "Local"
+    file_path_local = config[section_local]["file_path"]
+
+    section_gdrive = "Google Drive"
+    application_name = config[section_gdrive]["application_name"]
+
+    client_secret_file_path = "client_secret.json"
+
+    g = GoogleDrive(application_name, client_secret_file_path)
+    g.upload_file()
