431405405e0d1caaefc61c4a6d583c6c8073b0c0
Jeffrey Leung <jleung513@gmail.com>
2018-02-05 01:29:20 -0800

    [Slack Messenger] Move Logger class to separate module

--- modules/slack_messenger/slack_messenger.py
+++ modules/slack_messenger/slack_messenger.py
@@ -7,48 +7,8 @@ import json
 import time
 from slackclient import SlackClient
 
-class Logger:
-    """Outputs formatted log messages."""
-
-    # Change this to False to disable all output logging for this module.
-    print_logs = True
-
-    @classmethod
-    def __log(self, log_level, message):
-        """Outputs a formatted log message if logging is activated.
-        Parameters:
-        log_level -- String. Severity of the log message.
-        message -- String. Message to be logged.
-        """
-        if self.print_logs:
-            print(
-                    "[ " +
-                    time.strftime("%Y-%m-%d %H:%M:%S") +
-                    " | " +
-                    log_level +
-                    " ] " +
-                    message
-            )
-
-    @staticmethod
-    def debug(message):
-        """Outputs a debug level log message."""
-        Logger.__log("DEBUG  ", message)
-
-    @staticmethod
-    def info(message):
-        """Outputs an info level log message."""
-        Logger.__log("INFO   ", message)
-
-    @staticmethod
-    def success(message):
-        """Outputs a success level log message."""
-        Logger.__log("SUCCESS", message)
-
-    @staticmethod
-    def error(message):
-        """Outputs a error level log message."""
-        Logger.__log("ERROR  ", message)
+# Custom imports
+from logger import Logger
 
 class SlackMessenger:
 
