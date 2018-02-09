ab7d51df8b79f12038681ccac97b96410b2435fb
Jeffrey Leung <jleung513@gmail.com>
2018-02-09 04:03:40 -0800

    [Google API] Modify Gmail sender to be subclass
    This allows for more flexibility when adding more Google API classes.

--- modules/google_api/google_api.py
+++ modules/google_api/google_api.py
@@ -1,20 +1,21 @@
-# This Python 3 module provides a function to access the Gmail API in order
-# to send an email from a Gmail account.
+# This Python 3 module provides classes to access the Google API.
 
 import argparse
 import base64
 import os
 import time
 
-from email.mime.text import MIMEText
 from httplib2 import Http
 
-# Gmail API
+# Google API
 from apiclient.discovery import build
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 from oauth2client.service_account import ServiceAccountCredentials
+
+# Gmail
+from email.mime.text import MIMEText
 
 # Change this to True to enable output debug logging for this module.
 print_debug_logs = False
@@ -41,30 +42,31 @@ def log(log_level, message):
                 message
         )
 
-def create_message(sender, recipient, subject, message_text):
-    message = MIMEText(message_text)
-    message["from"] = sender
-    message["to"] = recipient
-    message["subject"] = subject
-    return {"raw":
-            base64.urlsafe_b64encode(
-                    message.as_string().encode()
-            ).decode("utf-8")
-    }
+class GoogleApi:
 
-class GmailSender:
+    _scope = ""
 
-    def __get_gmail_credentials(self):
+    def _get_credentials(self):
+        """Retrieves or generates credentials for Google account access.
+
+        Requires a client_secret.json file in the same directory. See README
+        for instructions to create it.
+
+        Arguments:
+        scope -- String. Google Authentication scope which allows for a specific
+            area of access.
+            https://developers.google.com/identity/protocols/googlescopes
+        """
         credential_path = os.path.join(
                 os.path.dirname(os.path.realpath(__file__)),
                 "generated_credentials.json"
         )
         store = Storage(credential_path)
+
         credentials = store.get()
         if not credentials or credentials.invalid:
             flow = client.flow_from_clientsecrets(
-                    "client_secret.json",
-                    "https://www.googleapis.com/auth/gmail.send"
+                "client_secret.json", self._scope
             )
             flow.user_agent = self.application_name
 
@@ -73,7 +75,23 @@ class GmailSender:
                     .parse_args()
             flags.noauth_local_webserver = True
             credentials = tools.run_flow(flow, store, flags)
+
         return credentials
+
+class GmailApi(GoogleApi):
+
+    _scope = "https://www.googleapis.com/auth/gmail.send"
+
+    def _create_message(self, sender, recipient, subject, message_text):
+        message = MIMEText(message_text)
+        message["from"] = sender
+        message["to"] = recipient
+        message["subject"] = subject
+        return {"raw":
+                base64.urlsafe_b64encode(
+                        message.as_string().encode()
+                ).decode("utf-8")
+        }
 
     def __init__(self, source_email, application_name=None):
         """Instantiates a GmailApi object and refreshes its credentials.
@@ -91,7 +109,7 @@ class GmailSender:
         self.source_email = source_email
         self.application_name = application_name
 
-        self.__get_gmail_credentials()
+        self._get_credentials()
 
     def send_email(self, target_email, subject, message):
         """Sends an email to the specific email address.
@@ -109,10 +127,10 @@ class GmailSender:
         log_debug("Mail subject: " + subject)
         log_debug("Mail message: " + message.replace("\n", "[newline]"))
 
-        http_auth = self.__get_gmail_credentials().authorize(Http())
+        http_auth = self._get_credentials().authorize(Http())
         service = build("gmail", "v1", http=http_auth)
 
-        mail = create_message(
+        mail = self._create_message(
                 self.source_email, target_email,
                 subject, message
         )
