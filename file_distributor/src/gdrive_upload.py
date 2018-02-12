31804d4440f4be0c17b2389262ebe8c5a11c7e74
Jeffrey Leung <jleung513@gmail.com>
2018-02-12 02:56:05 -0800

    [File Distributor] Use Google API module

--- file_distributor/src/gdrive_upload.py
+++ file_distributor/src/gdrive_upload.py
@@ -5,100 +5,14 @@
 # are generated from a file named client_secret.json and stored in a file
 # named generated_credentials.json. See the README for more details.
 
-import argparse
 import configparser
-import httplib2
-import json
 import os
-import sys
-import time
-
-# Google Drive API
-from apiclient.discovery import build
-from apiclient.http import MediaFileUpload
-from oauth2client import client
-from oauth2client import tools
-from oauth2client.file import Storage
 
 # Custom imports
 from logger import Logger
+from google_api import GoogleDriveApi
 
 config_filename = "file_distributor.cfg"
-
-class GoogleDrive:
-    """Wrapper for the Google Drive API."""
-
-    def __get_credentials(self):
-        """Refreshes Google Drive credentials, authorizing if necessary.
-
-        self.__application_name and self.__client_secret_file_path must have
-        been set.
-        The credentials will be saved to generated_credentials.json.
-
-        Returns OAuth credentials.
-        """
-        credential_path = os.path.join(
-                os.path.dirname(os.path.realpath(__file__)),
-                "generated_credentials.json"
-        )
-        store = Storage(credential_path)
-        credentials = store.get()
-        if not credentials or credentials.invalid:
-            scope = "https://www.googleapis.com/auth/drive"
-            flow = client.flow_from_clientsecrets(
-                    self.__client_secret_file_path, scope
-            )
-            flow.user_agent = self.__application_name
-
-            flags = argparse \
-                    .ArgumentParser(parents=[tools.argparser]) \
-                    .parse_args()
-            flags.noauth_local_webserver = True
-            credentials = tools.run_flow(flow, store, flags)
-            Logger.debug(
-                    "Google Drive credentials saved to [" +
-                    credential_path + "]"
-            )
-        return credentials
-
-    def __init__(self, application_name, client_secret_file_path):
-        """Initializes and pre-authenticates the Google Drive credentials."""
-        self.__application_name = application_name
-        self.__client_secret_file_path = client_secret_file_path
-
-        http_auth = self.__get_credentials().authorize(httplib2.Http())
-        self.__service = build("drive", "v3", http=http_auth)
-
-    def list_files(self):
-        """Retrieves the data of all files in the Google Drive account."""
-        Logger.debug("File and directory details:")
-
-        file_list = self.__service.files().list().execute()["files"]
-        for file in file_list:
-            print("  " + json.dumps(file))
-
-    def upload_file(self, file_path_local, file_name_gdrive, parent_dir_id=None):
-        """Uploads a file to a Google Drive account.
-
-        Parameters:
-        file_path_local -- String. Absolute path to the file to be uploaded.
-        file_name_gdrive -- String. Filename for the uploaded file in
-            Google Drive.
-        parent_dir_id -- String (optional). File ID for the parent directory
-            of the uploaded file. See README.md for instructions.
-        """
-        body = {
-                "name" : file_name_gdrive
-        }
-        if parent_dir_id is not None:
-            body["parents"] = [parent_dir_id]
-
-        self.__service.files().create(
-                body=body,
-                media_body=MediaFileUpload(file_path_local),
-        ).execute()
-
-        Logger.debug("File [" + file_path_local + "] uploaded to Google Drive.")
 
 def main():
     config = configparser.ConfigParser()
@@ -114,7 +28,7 @@ def main():
 
     client_secret_file_path = "client_secret.json"
 
-    g = GoogleDrive(application_name, client_secret_file_path)
+    g = GoogleDriveApi(application_name)
 
     # g.list_files()  # Can be used to find the FileId of a specific directory
 
