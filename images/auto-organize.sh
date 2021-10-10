951d3c187cfe2750003abd9721ff69845b936440
Jeffrey Leung <jleung513@gmail.com>
2021-10-09 22:55:28 -0700

    [Images] Add script to run multiple image scripts

---
+++ images/auto-organize.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+SCRIPTS_DIR="$(dirname "$0")"
+
+# Modify extensions and filetypes
+echo "Lowercasing extensions..."
+$SCRIPTS_DIR/lowercase-extensions
+echo "Standardizing JPG extensions..."
+$SCRIPTS_DIR/standardize-jpg
+echo "Converting PNG files to JPG..."
+$SCRIPTS_DIR/png-to-jpg
+
+# Modify filenames
+echo "Re-labeling images in sequential order..."
+$SCRIPTS_DIR/number-img
+
+# Modify image sizes
+echo "Resizing images to be approximately under 1 MB..."
+$SCRIPTS_DIR/resize-img-auto
+
+echo ""
+echo "____________________________________"
+echo ""
+echo "Auto-organized files:"
+$SCRIPTS_DIR/dimensions
